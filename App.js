ed7ee1d27f62e6224d102751d681ccdf46227c26
Hridoy <hriodykumarshil12@gmail.com>
2022-11-07 19:05:44 +0600

    React Native Components

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const handleInputChange = text => {
+    alert(text);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Creating first React Native App</Text>
+      <Text>Hello World!</Text>
+      <Text>My First React Native App</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        placeholder="I am placeholder"
+        onChangeText={text => handleInputChange(text)}
+      />
       <StatusBar style="auto" />
     </View>
   );
