6295a3556ed981c443a990a61ecb73b6ef6bedb1
Hridoy <hriodykumarshil12@gmail.com>
2022-11-07 20:15:31 +0600

    Creating and Connecting Redux Store

---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+import * as actionTypes from './actionTypes';
+
+const initState = {
+}
+
+export const rootReducer = (state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
