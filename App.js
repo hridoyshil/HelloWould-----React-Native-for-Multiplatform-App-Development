fcf537a21ccccf0f02ea3be86426207f88c60aa3
Hridoy <hriodykumarshil12@gmail.com>
2022-11-07 19:13:53 +0600

    Managing State with Text Input and Button

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const handleInputChange = text => {
-    alert(text);
-  }
+  const [inputValue, setInputValue] = useState("");
+  const [placeList, setPlaceList] = useState([]);
   return (
     <View style={styles.container}>
-      <Text style={{ color: "red" }}>Hello World!</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={{
+            width: "80%",
+            borderBottomWidth: 1,
+            borderColor: "green",
+            padding: 7
+          }}
+          placeholder="Add a Place.."
+          value={inputValue}
+          onChangeText={text => setInputValue(text)}
+        />
+        <Button
+          title="Add"
+          onPress={() => {
+            if (inputValue !== "") {
+              setPlaceList([...placeList, inputValue])
+            }
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -18,12 +37,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'column'
   },
-
-  textStyle: {
-    color: "red"
+  inputView: {
+    padding: 20,
+    width: "100%",
+    marginTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   }
-
 });
-
