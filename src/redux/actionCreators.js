3d8ec9580756c1194b4d8faf2f1687b2ea4fc849
Hridoy <hriodykumarshil12@gmail.com>
2022-11-14 11:00:28 +0600

    Using Google Firebase to Store Data
    Managing Data Using REST API

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,11 +1,15 @@
 import * as actionTypes from './actionTypes';
 
-export const addPlace = place => {
-    return {
-        type: actionTypes.ADD_PLACE,
-        payload: place
-    }
+export const addPlace = place => dispatch => {
+    fetch("https://my-places-dcd23-default-rtdb.firebaseio.com/places.json", {
+        method: "POST",
+        body: JSON.stringify(place)
+    })
+        .catch(error => console.log(error))
+        .then(response => response.json())
+        .then(data => console.log(data));
 }
+
 
 export const deletePlace = key => {
     return {
